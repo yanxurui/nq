b258eaf304f5eaecfcd6c426371a98df690ff469
yanxurui <617080352@qq.com>
2017-11-07 18:50:43 +0800

    format

--- tests/config.py
+++ tests/config.py
@@ -3,9 +3,9 @@ PREFIX = '/opt/nq/'
 HOST = 'http://127.0.0.1:8001'
 
 MYSQL = {
-	'HOST': '127.0.0.1',
-	'port': 3306,
-	'USRE': 'nq',
-	'PASSWORD': '123456',
-	'DATABASE': 'nq_test'
+    'HOST': '127.0.0.1',
+    'port': 3306,
+    'USRE': 'nq',
+    'PASSWORD': '123456',
+    'DATABASE': 'nq_test'
 }
